9861c63c9b0ac52f3e57277240b285cef18b3063
andrroy <andreas@royrvik.org>
2015-11-10 01:47:26 +0000

    Added initial db schema

---
+++ server/startup.js
@@ -0,0 +1,11 @@
+//Meteor.startup(function() {
+//
+//    var idRock, idPop, idTechno, id60s;
+//
+//    if(Meteor.users.find().count() === 0) {
+//        console.log("EMPTY USER COLLECTION. Users added: magnus (pass: a), andreas (pass: a)");
+//        Accounts.createUser({username: "magnus", email:"a@a.com", password: "a"});
+//        Accounts.createUser({username: "andreas",email: "aa@a.com", password: "a"});
+//    }
+//
+//});
